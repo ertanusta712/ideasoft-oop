241391e8b433ec261ef42034832c6fc52a0b9076
Ertan USTA <ertan.usta@ideasoft.com.tr>
2020-04-04 16:40:50 +0300

    OOP Course 2 Chapter 1

---
+++ course2/lib/Ship.php
@@ -0,0 +1,130 @@
+<?php
+
+/**
+ * Class Ship
+ */
+class Ship
+{
+
+    private $name;
+
+    private $weaponPower = 0;
+
+    private $jediFacotr = 0;
+
+    private $strength = 0;
+
+    private $underRepair;
+
+    public function __construct($name)
+    {
+        $this->name=$name;
+        $this->underRepair = mt_rand(1, 100) < 30;
+    }
+
+    public function sayHello()
+    {
+        echo 'hello';
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getNameAndSpecs($useShortFormat = false)
+    {
+        if ($useShortFormat) {
+            return sprintf(
+                '%s: %s/%s/%s',
+                $this->name,
+                $this->weaponPower,
+                $this->jediFactor,
+                $this->strength
+            );
+        } else {
+            return sprintf(
+                '%s: w:%s, j:%s, s:%s',
+                $this->name,
+                $this->weaponPower,
+                $this->jediFactor,
+                $this->strength
+            );
+        }
+    }
+
+    public function doesGivenShipHaveMoreStrength($givenShip)
+    {
+        return $givenShip->strength > $this->strength;
+    }
+
+    /**
+     * @return int
+     */
+    public function getStrength()
+    {
+        return $this->strength;
+    }
+
+    /**
+     * @param int $strength
+     */
+    public function setStrength($strength)
+    {
+        $this->strength = $strength;
+    }
+
+    /**
+     * @return int
+     */
+    public function getWeaponPower()
+    {
+        return $this->weaponPower;
+    }
+
+    /**
+     * @param int $weaponPower
+     */
+    public function setWeaponPower($weaponPower)
+    {
+        $this->weaponPower = $weaponPower;
+    }
+
+    /**
+     * @return int
+     */
+    public function getJediFacotr()
+    {
+        return $this->jediFacotr;
+    }
+
+    /**
+     * @param int $jediFacotr
+     */
+    public function setJediFacotr($jediFacotr)
+    {
+        $this->jediFacotr = $jediFacotr;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isUnserRepair()
+    {
+        return !$this->underRepair;
+    }
+
+
+
+}
---
+++ course2/play.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once __DIR__.'lib/Ship.php';
+
+/**
+ * @param Ship $someShip
+ */
+function printShipSummary($someShip)
+{
+    echo 'Ship Name: ' . $someShip->getName();
+    echo '<hr/>';
+    $someShip->sayHello();
+    echo '<hr/>';
+    $someShip->getName();
+    echo '<hr>';
+    echo $someShip->getNameAndSpecs(false);
+    echo '<hr/>';
+    echo $someShip->getNameAndSpecs(true);
+}
+
+$myShip = new Ship();
+$myShip->name = 'ertan';
+$myShip->weaponPower = 10;
+
+$otherShip = new Ship();
+$otherShip->name = 'usta';
+$otherShip->weaponPower = 5;
+$otherShip->strength = 50;
+
+printShipSummary($myShip);
+echo '<hr/>';
+printShipSummary($otherShip);
+
+
+echo '<hr/>';
+if ($myShip->doesGivenShipHaveMoreStrength($otherShip)) {
+    echo $otherShip->name.' has more strength';
+} else {
+    echo $myShip->name.' has more strength';
+}
