27920bf843f9b0c5b920b5bcaacd476b6ad66f40
Ertan USTA <ertan.usta@ideasoft.com.tr>
2020-04-04 14:34:12 +0300

    OOP Course 1 Chapter 3

--- play.php
+++ play.php
@@ -5,10 +5,33 @@ class Ship
 
     public $name;
 
+    public $weaponPower = 0;
 
+    public $jediFacotr = 0;
+
+    public $strength = 0;
+
+    public function sayHello()
+    {
+        echo 'hello';
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
 }
 
 $myShip = new Ship();
 $myShip->name = 'ertan';
-
+$myShip->weaponPower = 10;
 echo $myShip->name;
+echo '<hr>';
+$myShip->sayHello();
+echo '<hr>';
+$myShip->getName();
+echo '<hr>';
+var_dump($myShip->weaponPower);
